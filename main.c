0a1159a7c2e69e4f1f6d7d7fbfe0b645d56798ef
alex <alexander.schicktanz@web.de>
2025-03-27 03:02:07 +0100

    backup2

--- main.c
+++ main.c
@@ -4,99 +4,115 @@
 #include <stdio.h>
 
 #define LIGHTBLUE (Color){ 0, 121, 241, 155 }
+
 int infoStartX = -500;
 int taskStartX = 50;
+int quellenStartX = 600;
 int startY = 300;
 int endX = 450;
-int endY =250;
-
+int endY = 250;
 int width, height;
 
+typedef enum {
+    MENU,
+    INFO,
+    TEST,
+    QUELLEN
+} Screen;
+
 void setSize(int w, int h) {
-    width = w*4;
-    height = h*4;
-}
-
-
-void Quellen(){
-
-}
-
-void Info(){
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground((Color){33,100,119});
-        DrawRectangle(0, 0, width, height, (Color){33,100,119,255});
-        DrawText("Informationen", 100, 100, 40, WHITE);
-        DrawRectangleRec((Rectangle){50, 500, 100, 40}, GRAY);
-        EndDrawing();
-        if (CheckCollisionPointRec(GetMousePosition(), (Rectangle){{50, 500, 100, 40}}) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-            printf("Back!\n");
-            return;
-        }
-    }
-}
-
-void Test(){
-    while(!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground((Color){33,100,119});
-        DrawRectangle(0, 0, width, height, (Color){33,100,119,255});
-        DrawText("Aufgaben", 100, 100, 40, WHITE);
-        DrawRectangleRec((Rectangle){50, 500, 100, 40}, GRAY);
-        EndDrawing();
-        if (CheckCollisionPointRec(GetMousePosition(), (Rectangle){{50, 500, 100, 40}}) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-            return;
-        }
-    }
-}
-
-bool ButtonRect(Rectangle rec, Color col1,Color col2, const char* text, Font font) {
-    bool hover = CheckCollisionPointRec(GetMousePosition(), rec);
-    DrawRectangleRec(rec, hover?col1:col2);
-    DrawTextEx(font,text, (Vector2){rec.x+rec.width/2-MeasureText(text, rec.width/40)/2, rec.y+rec.height/2-20}, rec.width/40, 10, WHITE);
-    return (hover && IsMouseButtonPressed(MOUSE_LEFT_BUTTON));
+    width = w * 4;
+    height = h * 4;
 }
 
 int main(void) {
     InitWindow(width, height, "KonDiv");
-    printf("Width: %d\nHeight: %d\n", width, height);
-    SetWindowState(FLAG_WINDOW_UNDECORATED|FLAG_WINDOW_MAXIMIZED);
+    SetWindowState(FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_MAXIMIZED);
     SetTargetFPS(60);
+
     Font def = LoadFontEx("./resources/ViaodaLibre-Regular.ttf", 256, NULL, 255);
     GuiSetFont(def);
-    if(!def.texture.id) {
+    if (!def.texture.id) {
         printf("Font not loaded!\n");
         return 1;
     }
-    Vector2 size = MeasureTextEx(def, "Von Tintenfischen und Vögeln: Konvergenz und Divergenz in der Evolution", 40, 10);
+
     GuiSetStyle(DEFAULT, TEXT_SIZE, 40);
+    Screen currentScreen = MENU;
+
     while (!WindowShouldClose()) {
         Vector2 mousePos = GetMousePosition();
         BeginDrawing();
-        ClearBackground((Color){33,100,119});
-        DrawRectangle(0, 0, width, height, (Color){33,100,119,255});
-        DrawTextEx(def,"Von Tintenfischen und Vögeln:", (Vector2){400, 100}, 160, 40, WHITE);
-        DrawTextEx(def,"Konvergenz und Divergenz in der Evolution", (Vector2){400, 250}, 160, 40, WHITE);
-        /*if(ButtonRect((Rectangle){0, height/2, width/2, height/2}, BLUE, LIGHTBLUE,"Informationen", def)) {
-            printf("Left!\n");
+        ClearBackground((Color){33, 100, 119});
+        DrawRectangle(0, 0, width, height, (Color){33, 100, 119, 255});
+
+        switch (currentScreen) {
+            case MENU:
+                DrawTextEx(def, "Von Tintenfischen und Vögeln:", (Vector2){400, 100}, 160, 40, WHITE);
+                DrawTextEx(def, "Konvergenz und Divergenz in der Evolution", (Vector2){400, 250}, 160, 40, WHITE);
+                
+                DrawRectangle(width / 2 + infoStartX, startY, endX, endY, BLUE);
+                DrawRectangle(width / 2 + taskStartX, startY, endX, endY, RED);
+                DrawRectangle(width / 2 + quellenStartX, startY, endX, endY, LIGHTBLUE);
+                DrawText("Informationen", width / 2 + infoStartX, startY - 20, 20, WHITE);
+                DrawText("Aufgaben", width / 2 + taskStartX, startY - 20, 20, WHITE);
+                DrawText("Quellen", width / 2 + quellenStartX, startY - 20, 20, WHITE);
+                
+                if (CheckCollisionPointRec(mousePos, (Rectangle){width / 2 + infoStartX, startY, endX, endY}) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                    currentScreen = INFO;
+                }
+                else if (CheckCollisionPointRec(mousePos, (Rectangle){width / 2 + taskStartX, startY, endX, endY}) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                    currentScreen = TEST;
+                }
+                else if (CheckCollisionPointRec(mousePos, (Rectangle){width / 2 + quellenStartX, startY, endX, endY}) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                    currentScreen = QUELLEN;
+                }
+                break;
+
+            case INFO:
+                DrawText("Informationen", 100, 100, 40, WHITE);
+                DrawRectangleRec((Rectangle){50, 500, 100, 40}, GRAY);
+                if (CheckCollisionPointRec(mousePos, (Rectangle){50, 500, 100, 40}) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                    currentScreen = MENU;
+                }
+                DrawTextEx(def, "Konvergente Evolution (Analogie)\n"
+"Definition: das Entstehen ähnlicher Merkmale bei nicht miteinander verwandten Arten, Entwicklung von analogen Merkmalen bei nicht näher verwandten Arten\n"
+"Ursache: im Laufe der Evolution durch Anpassung an eine ähnliche funktionale Anforderung und ähnliche Umweltbedingungen (ähnliche ökologische Nischen) ausgebildet, Wechselspiel von ähnlichem Selektionsdruck und Entwicklungskorridoren*1, gleiche Selektionsfaktoren, die zu vergleichbaren Anpassungen geführt haben\n"
+"Folge:\n"
+"Beispiele: Fische und Wale (Flossen), Vögel und Insekten (Flügel), Dornen und Stacheln*2, Tintenfische und Vögel (Schnabel)\n"
+"ähnliche Merkmale lassen sich direkt auf ihre Funktion zurückführen und liefern nicht unbedingt einen Rückschluss auf nahe Verwandtschaft zwischen zwei Arten\n"
+"Analoge Organe ähneln sich nicht nur in der Funktion, sondern teilweise auch äußerlich, teilweise sogar (oberflächlich) anatomisch, ABER: unabhängig voneinander entstanden\n"
+"Analogie ist das Ergebnis von Konvergenz\n"
+"*1 Bandbreite an möglichen Phänotypen, die ein bestimmter Genotyp unter verschiedenen Umweltbedingungen annehmen kann.\n"
+"*2 In der Botanik gibt es ebenfalls analoge Bildungen der Pflanzen. So werden von Laien Stacheln oft mit Dornen verwechselt. Im Gegensatz zu den Dornen werden Stacheln aber nur aus den oberen Zellschichten (Epidermis, Rindengewebe) gebildet. Sie sind also nur Oberflächenstrukturen (Emergenzen). Dornen sind hingegen Umbildungen der Blätter oder der Sprossachse.\n", (Vector2){100, 400}, 40, 10, WHITE);
+            DrawTextEx(def, "Divergente Evolution\n"
+"Definition: das Entstehen unterschiedlicher Merkmale bei eng verwandten Arten, Form der Artbildung\n"
+"Ursache: Starke inner- und außerartliche Konkurrenz, verschiedene Evolutionsfaktoren\n"
+"Folge: Divergenz führt zur Besetzung und Bildung neuer ökologischer Nischen und damit zur adaptiven Radiation. Im Endeffekt entstehen so neue Arten (Speziation), Individuen werden an ihre neuen, sich voneinander unterscheidenden Lebensbedingungen individuell angepasst\n"
+"Beispiele: Darwinfinken, Knochenbau bei Säugetieren\n"
+"Trotz der divergenten Entwicklungen bleiben Merkmale aufgrund der gemeinsamen Abstammung vergleichbar. Diese grundsätzliche Übereinstimmung wird als Homologie bezeichnet\n", (Vector2){100, 900}, 40, 10, WHITE);
+            break;
+            
+            case TEST:
+                DrawText("Aufgaben", 100, 100, 40, WHITE);
+                DrawRectangleRec((Rectangle){50, 500, 100, 40}, GRAY);
+                if (CheckCollisionPointRec(mousePos, (Rectangle){50, 500, 100, 40}) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                    currentScreen = MENU;
+                }
+                break;
+            
+            case QUELLEN:
+                DrawText("Quellen", 100, 100, 40, WHITE);
+                DrawRectangleRec((Rectangle){50, 500, 100, 40}, GRAY);
+                if (CheckCollisionPointRec(mousePos, (Rectangle){50, 500, 100, 40}) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                    currentScreen = MENU;
+                }
+                DrawTextEx(def, "https://studyflix.de/biologie/konvergenz-6333\n"
+"https://simpleclub.com/lessons/biologie-divergenz\n"
+"https://de.wikipedia.org/wiki/Divergenz_(Biologie)\n"
+"https://www.biologie-seite.de/Biologie/Konvergenz_(Biologie)\n", (Vector2){100, 400}, 40, 10, WHITE);
+                break;
         }
-        if(ButtonRect((Rectangle){width/2, height/2, width/2, height/2}, BLUE, LIGHTBLUE,"Aufgaben", def)) {
-            printf("Right!\n");
-        }*/
-        if (CheckCollisionPointRec(mousePos, (Rectangle){width/2 + infoStartX, startY, endX, endY})&&IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-            Info();
-        }
-                // Aufgaben-Button Bereich (rot)
-        else if (CheckCollisionPointRec(mousePos, (Rectangle){width/2 + taskStartX, startY, endX, endY})&&IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-            Test();
-        }
-        DrawRectangle(width/2 + infoStartX, startY, endX, endY, BLUE);
-        DrawRectangle(width/2 + + taskStartX, startY, endX, endY, RED);
-        
-        DrawText("Informationen", width/2 + infoStartX, startY-20, 20, WHITE);
-        DrawText("Aufgaben", width/2 + taskStartX, startY-20, 20, WHITE);
-        //if(GuiButton((Rectangle){100, 500, 300, 100}, "Click me!")) printf("Clicked!\n");
         EndDrawing();
     }
 
